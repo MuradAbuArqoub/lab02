b36eb9cfe02bda79cdeb941917c87cec9e471313
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-08-03 17:08:33 +0300

    edited

--- app.js
+++ app.js
@@ -1,63 +1,66 @@
 alert("hello hello and welcome")
-var name = prompt("what name do you go by?") 
+var name = prompt("what name do you go by?")
 
 
-alert(name + " huh, never heard of it" )
-alert("anyway " + name + " lets play a guessing game shall we?" )
+alert(name + " huh, never heard of it")
+alert("anyway " + name + " lets play a guessing game shall we?")
 
 alert("yes or no responses will only be accepted")
 let points = 0;
 
+
 let q1 = prompt("Do you think my name starts with A").toLowerCase();
-if(q1 == "yes"){ 
-    alert("Nope"); 
+if (q1 == "yes") {
+    alert("Nope");
     console.log("False");
 
-} else if(q1 == "no"){ 
+} else if (q1 == "no") {
     alert("ayyy");
     console.log("True");
     points++;
 }
-  
+
 let q2 = prompt("Am i a cat person in ur opinion :3?").toLowerCase();
-if(q2 == "yes"){
+if (q2 == "yes") {
     alert("hehe");
     console.log("True");
     points++;
-} else if(q2 == "no"){ 
-    alert("noh"); 
+} else if (q2 == "no") {
+    alert("noh");
     console.log("False");
 }
 
 let q3 = prompt("Do you think am older than 21?").toLowerCase();
-if(q3 == "yes"){
+if (q3 == "yes") {
     alert("yup");
     console.log("True");
     points++;
-} else if(q3 == "no"){ 
-    alert("nope"); 
+} else if (q3 == "no") {
+    alert("nope");
     console.log("False");
 }
 
 let q4 = prompt("Do you think i would pick coffee over tea?").toLowerCase();
-if(q4 == "yes"){
+if (q4 == "yes") {
     alert("naaaah");
     console.log("false");
     points++;
-} else if(q4 == "no"){ 
-    alert("hell ya boi"); 
+} else if (q4 == "no") {
+    alert("hell ya boi");
     console.log("yes");
 }
 
 let q5 = prompt("Do you think am taler than 185 cm?").toLowerCase();
-if(q5 == "yes"){
+if (q5 == "yes") {
     alert("yes i am a tall boi");
     console.log("yos");
     points++;
-} else if(q5 == "no"){ 
-    alert("guess again tall gang skrt skrt"); 
+} else if (q5 == "no") {
+    alert("guess again tall gang skrt skrt");
     console.log("nope");
 }
+
+alert(name + ", your total score is " + points);
 
 alert("Now here is another type of guessing game")
 
@@ -67,32 +70,58 @@ let counter = 0;
 let numberOfTries = 4;
 let randomNumber = Math.floor(Math.random(10) * userNumber + 1);
 
-
-
-    for(let attempt = 0; attempt < numberOfTries; attempt ++){
-        let answer = Number(prompt("guess the random number that comes between 1-10"));
-        if(answer > randomNumber){
-            alert("too high try a smaller number")    
-        }
-        else if (answer < randomNumber){
-            alert("Too low, guess again!")
-        }
-
-        if(answer == randomNumber){
-            console.log("mmm yes")
-            alert("well done")
-            alert("the random number was " + randomNumber)
-            alert("welp good job "+ name )
-            break;
-        }
-        if(attempt == 3 ){
-            console.log("mmm no")
-            alert("sad")
-            alert("tough luck "+ name )
-            break;
-        }
+for (let attempt = 0; attempt < numberOfTries; attempt++) {
+    let answer = Number(prompt("guess the random number that comes between 1-10"));
+    if (answer > randomNumber) {
+        alert("too high try a smaller number")
+    }
+    else if (answer < randomNumber) {
+        alert("Too low, guess again!")
     }
 
-    // 1- all good
-    // 2- iv had some questions but i found everything on internet
-    // 3- it toke me some time by some time i mean a whole day because am still not used to JS or HTML
+    if (answer == randomNumber) {
+        console.log("mmm yes")
+        alert("well done")
+        alert("the random number was " + randomNumber)
+        alert("welp good job " + name)
+        break;
+    }
+    if (attempt == 3) {
+        console.log("mmm no")
+        alert("sad")
+        alert("tough luck " + name)
+        break;
+    }
+}
+
+// 1- all good
+// 2- iv had some questions but i found everything on internet
+// 3- it toke me some time by some time i mean a whole day because am still not used to JS or HTML
+
+alert("now here is another guessing game")
+let numberOfTriesSecond = 6;
+let favFood = ['sushi', 'pizza', 'smoked stake'];
+
+for (let i = 0; i < favFood.length; i++) {
+    let answer2 = prompt("guess at least on of my favorit dishes\nshawerma, burger, pizza, dawali, sushi, smoked stake, salad, bbq, fired chicken, falafil");
+    if (answer2 === favFood[i]) {
+        console.log("good job");
+        alert("well done my favorit dishes are " + favFood)
+        break;
+    }
+    else {
+        console.log("nope");
+        alert("nah not really")
+    }
+
+    if (counter == numberOfTriesSecond) {
+        console.log("sorry you used all your chances");
+        alert("sorry you used all your chances")
+        alert("my favorit dishes are " + favFood)
+        break;
+    }
+}
+
+
+
+
